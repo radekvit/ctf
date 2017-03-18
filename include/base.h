113ef787245f47fd248ce9e71d60e5564ff0c8a6
Radek Vit <radekvitr@gmail.com>
2017-03-18 17:02:22 +0100

    throwing exception when non-EOI Symbol has empty name

--- include/base.h
+++ include/base.h
@@ -38,8 +38,11 @@ class Symbol {
 
  public:
   Symbol(Type type, const string &name = "", const string &atr = "")
-      : type_(type), name_(name), attribute_(atr) {}
-  Symbol(const string &name = "", const string &atr = "")
+      : type_(type), name_(name), attribute_(atr) {
+        if (type != Symbol::Type::EOI && name == "")
+          throw std::invalid_argument("Empty name when constructing non-EOI Symbol.");
+      }
+  Symbol(const string &name, const string &atr = "")
       : Symbol(Type::UNKNOWN, name, atr) {}
   ~Symbol() = default;
 
